743c6e858440465bf391ab78b81bf8776bf0003e
Athari31 <138482333+Athari31@users.noreply.github.com>
2023-08-23 12:18:07 +0400

    Initial commit

---
+++ bin/dart_application_13.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application_13/dart_application_13.dart' as dart_application_13;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_13.calculate()}!');
+}
